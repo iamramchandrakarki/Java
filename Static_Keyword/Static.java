9eec35ae97fb8f07abbe4fb6465a32088d4a2422
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-12-01 10:32:37 +0545

    flow

--- Static_Keyword/Static.java
+++ Static_Keyword/Static.java
@@ -1,23 +1,40 @@
-class Demo{
-    static int a, b, c;
-    int x, y, z; // Instance Variable : Memory is created when we create object
+// class Demo{
+//     static int a, b, c;
+//     int x, y, z; // Instance Variable : Memory is created when we create object
+//
+//     static {
+//         System.out.println("Static init block: ");
+//         a = 10;
+//         b = 20;
+//         c = 30;
+//     }
+//
+//     static void display(){
+//         System.out.println("a : " + a);
+//         System.out.println("b : " + b);
+//         System.out.println("c : " + c);
+//     }
+//
+// }
+// public class Static{
+//     public static void main(String[] args){
+//         Demo.display();
+//     }
+// }
+//
+
+public class Static{
+    static int a;
 
     static {
-        System.out.println("Static init block: ");
         a = 10;
-        b = 20;
-        c = 30;
+        System.out.println("This is Static block: ");
+        System.out.println("a = " + a);
     }
 
-    static void display(){
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-        System.out.println("c : " + c);
-    }
-
-}
-public class Static{
     public static void main(String[] args){
-        Demo.display();
+        System.out.println("Main method");
     }
 }
+
+// First run static block and then main method
